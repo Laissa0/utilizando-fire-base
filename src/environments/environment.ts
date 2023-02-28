074da938bb0311380c5b0670c7815ca2b3a9bebb
home-laissa-soares <laissa.soares@geraestec.com.br>
2023-02-28 20:41:44 -0300

    Atulização do projeto

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase: {
+      apiKey: "AIzaSyA4QHhmB4-eouyRyXCPBQnTRsp3qMe21lk",
+      authDomain: "applicationangularfirebase.firebaseapp.com",
+      projectId: "applicationangularfirebase",
+      storageBucket: "applicationangularfirebase.appspot.com",
+      messagingSenderId: "1084121674935",
+      appId: "1:1084121674935:web:1c0672369e211b3c198932",
+      measurementId: "G-WGEKXV5MCZ"
+    }
+  };
